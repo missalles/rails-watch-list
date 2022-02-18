00527ea811426de62876bf0c8a5a1eeeeb5414f2
Lydia Alles <lydia-alles@web.de>
2022-02-18 12:21:27 +0100

    Add Article scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,5 @@
 Rails.application.routes.draw do
-  get 'bookmark/home'
-  get 'list/home'
-
-  resources :lists, only: [:index, :new, :show, :create] do
-    resources :bookmarks, only: [:new, :create]
-  end
-  resources :bookmarks, only: :destroy
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'articles#index'
+  resources :articles, except: :index
+  #don't forget to replace `articles_path` by `root_path` on all the "Back" `link_to`
 end
